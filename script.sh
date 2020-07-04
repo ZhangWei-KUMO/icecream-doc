2db5ed3a75fb5b5d560461505eb43896ee28da10
ZhangWei-KUMO <kanseefoil@gmail.com>
2020-07-04 15:58:21 +0800

    add

--- script.sh
+++ script.sh
@@ -1,14 +1,3 @@
 #! /usr/bin/env bash
-image=icecream:0.0.1
-name=icecream
-port=7600
-docker stop $name
-docker rm $name
-rm package-lock.json
-rm yarn.lock
-rm -rf dist/
-
 git pull origin master
 yarn prod
-docker build -t $image .
-docker run --name $name -p $port:80 -d $image
